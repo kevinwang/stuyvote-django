64383b52501acb9651c5480765d11a49e9a81e47
Kevin Wang <born2run5000@yahoo.com>
2012-06-14 17:44:57 -0400

    Add Election model and relate Candidates and Votes to Elections.

--- vote/admin.py
+++ vote/admin.py
@@ -1,15 +1,19 @@
 from vote.models import *
 from django.contrib import admin
 
+class ElectionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'grade', 'can_choose_two_candidates')
+
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('osis', 'grade', 'has_voted')
+    list_display = ('osis', 'grade')
 
 class CandidateAdmin(admin.ModelAdmin):
-    list_display = ('name', 'grade', 'total_votes')
+    list_display = ('name', 'election', 'total_votes')
 
 class VoteAdmin(admin.ModelAdmin):
-    list_display = ('student', 'choice_1', 'choice_2')
+    list_display = ('student', 'election', 'choice_1', 'choice_2')
 
+admin.site.register(Election, ElectionAdmin)
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Candidate, CandidateAdmin)
 admin.site.register(Vote, VoteAdmin)
--- vote/models.py
+++ vote/models.py
@@ -2,21 +2,26 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from django.forms import ModelForm
 
+class Election(models.Model):
+    name = models.CharField(max_length=200)
+    grade = models.IntegerField()
+    can_choose_two_candidates = models.BooleanField()
+
+    can_choose_two_candidates.boolean = True;
+
+    def __unicode__(self):
+        return self.name;
+
 class Student(models.Model):
     osis = models.IntegerField()
     grade = models.IntegerField()
-
-    def has_voted(self):
-        return Vote.objects.filter(student=self).count() > 0
-
-    has_voted.boolean = True
 
     def __unicode__(self):
         return str(self.osis)
 
 class Candidate(models.Model):
     name = models.CharField(max_length=200)
-    grade = models.IntegerField()
+    election = models.ForeignKey(Election)
 
     def total_votes(self):
         return Vote.objects.filter(choice_1=self).count() + Vote.objects.filter(choice_2=self).count()
@@ -26,14 +31,17 @@ class Candidate(models.Model):
 
 class Vote(models.Model):
     student = models.ForeignKey(Student)
+    election = models.ForeignKey(Election)
     choice_1 = models.ForeignKey(Candidate, related_name='vote_choice_1')
     choice_2 = models.ForeignKey(Candidate, related_name='vote_choice_2', blank=True, null=True)
 
     def clean(self):
+        if self.election.grade != 0 and self.student.grade != self.election.grade:
+            raise ValidationError('Student cannot vote in this election.')
         if self.choice_1 == self.choice_2:
             self.choice_2 = None
-        if self.choice_1 is not None and self.choice_1.grade != self.student.grade or self.choice_2 is not None and self.choice_2.grade != self.student.grade:
-            raise ValidationError('Students can only vote for candidates in their own grade.')
+        if self.choice_1.election != self.election or self.choice_2 is not None and self.choice_2.election != self.election:
+            raise ValidationError('One or more selected candidates do not correspond with this election.')
 
     def __unicode__(self):
         return self.student.__unicode__() + ': ' + self.choice_1.__unicode__() + (', ' + self.choice_2.__unicode__() if self.choice_2 != None else '')

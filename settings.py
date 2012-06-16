21ce2d673bb79125402e84fde91945630f838fed
Kevin Wang <born2run5000@yahoo.com>
2012-06-16 18:43:44 -0400

    Enable admin documentation, add docutils to requirements file.

--- settings.py
+++ settings.py
@@ -114,7 +114,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
-    #'django.contrib.admindocs',
+    'django.contrib.admindocs',
     'vote',
 )
 
